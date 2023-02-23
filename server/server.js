38ea877df3f9f6c8e30df3c9f5afd81a0ba9313d
Erosi1 <93683494+Erosi1@users.noreply.github.com>
2023-02-23 21:23:37 +0100

    added server

--- server/server.js
+++ server/server.js
@@ -1,23 +1,29 @@
 const express=require('express');
 const morgan=require('morgan');
 const bodyParser=require('body-parser');
+const mongoose=require('mongoose');
+const dotenv=require('dotenv');
+const User=require('./models/user.js')
+dotenv.config();
+
  
 
 const app= express()
+mongoose.connect(process.env.DATABASE,{useNewUrlParser:true,useUnifiedTopology:true},(err)=>{
+    if(err){
+        console.log(err);
+    }else {
+        console.log("Connected to the database");
+    }
+})
 //MIDDLEWARE
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
-//Retrieve data from the server
-app.get("/",(req,res)=>{
-    res.json("Hello amazon");
-});
-//send data from frontend to backend
-app.post("/",(req,res)=>
-{
-    console.log(req.body.name);
-}
-);
+//require apis
+const productRoutes=require('./routes/product');
+app.use("/api",productRoutes);
+
 
 app.listen(4000,(err)=>{
     if(err){
