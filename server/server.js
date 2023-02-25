591e406a2cca85787e7683fc5503767007a86790
Erosi1 <93683494+Erosi1@users.noreply.github.com>
2023-02-25 20:10:48 +0100

    added api methods

--- server/server.js
+++ server/server.js
@@ -3,26 +3,32 @@ const morgan=require('morgan');
 const bodyParser=require('body-parser');
 const mongoose=require('mongoose');
 const dotenv=require('dotenv');
-const User=require('./models/user.js')
+const User=require('./models/user')
 dotenv.config();
 
  
 
 const app= express()
-mongoose.connect(process.env.DATABASE,{useNewUrlParser:true,useUnifiedTopology:true},(err)=>{
-    if(err){
+mongoose.set('strictQuery', true);
+mongoose.connect(process.env.DATABASE,
+err => {
+    if (err) {
         console.log(err);
-    }else {
+    } else {
         console.log("Connected to the database");
     }
-})
+});
 //MIDDLEWARE
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 //require apis
 const productRoutes=require('./routes/product');
+const categoryRoutes=require('./routes/category');
+const ownerRoutes=require('./routes/owner');
 app.use("/api",productRoutes);
+app.use("/api",categoryRoutes);
+app.use("/api",ownerRoutes);
 
 
 app.listen(4000,(err)=>{
